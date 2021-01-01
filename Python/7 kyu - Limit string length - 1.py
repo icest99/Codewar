904c7e962184734445daef89e53fda6c7ca19635
icest99 <playicest@gmail.com>
2021-01-01 19:40:14 +0700

    Python Practice Codewar Day#5

---
+++ Python/7 kyu - Limit string length - 1.py
@@ -0,0 +1,12 @@
+# Description:
+# The function must return the truncated version of the given string up to the given limit followed by "..." if the result is shorter than the original. Return the same string if nothing was truncated.
+
+# Example:
+
+# solution('Testing String', 3) --> 'Tes...'
+# solution('Testing String', 8) --> 'Testing ...'
+# solution('Test', 8)           --> 'Test'
+
+
+def solution(st, limit): 
+    return (st[0:limit] + "...") if (limit < len(st)) else st
